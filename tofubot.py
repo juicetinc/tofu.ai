d2df6efd7b809bf99fd37e4ad98ea5b37e8b0378
Justin Celeste <justinceleste@juicetin-macbook-air.local>
2024-07-15 10:47:14 -0700

    added tofubot script

---
+++ tofubot.py
@@ -0,0 +1,31 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+# load environment variables from .env file
+load_dotenv()
+
+# set up the necessary intents
+intents = discord.Intents.default()
+intents.message_content = True  # Enable the message content intent if you need to read message content
+
+# initializes the discrod client with intents
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+# get the token from the environment variable
+token = os.getenv('DISCORD_TOKEN')
+
+# run the client with the token
+client.run(token)
